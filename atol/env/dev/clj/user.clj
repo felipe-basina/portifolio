f494ce3a82dfe044756e2155a042202124b28499
felipe-basina <felipe.basina@gmail.com>
2021-04-06 20:24:33 -0300

    Adicionado estrutura do projeto.

---
+++ atol/env/dev/clj/user.clj
@@ -0,0 +1,63 @@
+(ns user
+  "Userspace functions you can run by default in your local REPL."
+  (:require
+   [atol.config :refer [env]]
+    [clojure.pprint]
+    [clojure.spec.alpha :as s]
+    [expound.alpha :as expound]
+    [mount.core :as mount]
+    [atol.core :refer [start-app]]
+    [atol.db.core]
+    [conman.core :as conman]
+    [luminus-migrations.core :as migrations]))
+
+(alter-var-root #'s/*explain-out* (constantly expound/printer))
+
+(add-tap (bound-fn* clojure.pprint/pprint))
+
+(defn start
+  "Starts application.
+  You'll usually want to run this on startup."
+  []
+  (mount/start-without #'atol.core/repl-server))
+
+(defn stop
+  "Stops application."
+  []
+  (mount/stop-except #'atol.core/repl-server))
+
+(defn restart
+  "Restarts application."
+  []
+  (stop)
+  (start))
+
+(defn restart-db
+  "Restarts database."
+  []
+  (mount/stop #'atol.db.core/*db*)
+  (mount/start #'atol.db.core/*db*)
+  (binding [*ns* (the-ns 'atol.db.core)]
+    (conman/bind-connection atol.db.core/*db* "sql/queries.sql")))
+
+(defn reset-db
+  "Resets database."
+  []
+  (migrations/migrate ["reset"] (select-keys env [:database-url])))
+
+(defn migrate
+  "Migrates database up for all outstanding migrations."
+  []
+  (migrations/migrate ["migrate"] (select-keys env [:database-url])))
+
+(defn rollback
+  "Rollback latest database migration."
+  []
+  (migrations/migrate ["rollback"] (select-keys env [:database-url])))
+
+(defn create-migration
+  "Create a new up and down migration file with a generated timestamp and `name`."
+  [name]
+  (migrations/create name (select-keys env [:database-url])))
+
+

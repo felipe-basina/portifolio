153f0b31ef8d8a3ca1ae0bc7885c34c6f6d9f2c8
felipe-basina <felipe.basina@gmail.com>
2021-04-06 21:40:17 -0300

    Adicionado operacoes get/post para testes.

--- atol/src/clj/atol/routes/home.clj
+++ atol/src/clj/atol/routes/home.clj
@@ -1,11 +1,11 @@
 (ns atol.routes.home
   (:require
-   [atol.layout :as layout]
-   [atol.db.core :as db]
-   [clojure.java.io :as io]
-   [atol.middleware :as middleware]
-   [ring.util.response]
-   [ring.util.http-response :as response]))
+    [atol.layout :as layout]
+    [atol.db.core :as db]
+    [clojure.java.io :as io]
+    [atol.middleware :as middleware]
+    [ring.util.response]
+    [ring.util.http-response :as response]))
 
 (defn home-page [request]
   (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
@@ -13,10 +13,20 @@
 (defn about-page [request]
   (layout/render request "about.html"))
 
+(defn test-page [request]
+  (layout/render request "test.html"))
+
+(defn say-hi [request]
+  (let [someone (get (:form-params request) "name")
+        someone (if (not-empty someone) someone "there!")]
+    (layout/render request "test.html" {:name someone})))
+
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
-   ["/about" {:get about-page}]])
+   ["/about" {:get about-page}]
+   ["/test" {:get test-page}]
+   ["/sayHi" {:post say-hi}]])
 

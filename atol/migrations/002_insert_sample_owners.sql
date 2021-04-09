b8b3b423c64900eaa7d14f0b9daa0c9edffea222
felipe-basina <felipe.basina@gmail.com>
2021-04-09 19:07:33 -0300

    Adicionado migracoes para tabela de usuarios.

---
+++ atol/migrations/002_insert_sample_owners.sql
@@ -0,0 +1,6 @@
+INSERT INTO t_owner (owner_name, email, owner_pass)
+values ('John Doe', 'john.doe@domain.com',
+        'bcrypt+sha512$6a97cbaa33d1476b8342d4f7146fc8a8$12$9e2366ead1b45135f12ccd4674f8b907bf6fb2e84a6de6d3');
+INSERT INTO t_owner (owner_name, email, owner_pass)
+values ('Mary Pops', 'mary.pops@domain.net',
+        'bcrypt+sha512$6a97cbaa33d1476b8342d4f7146fc8a8$12$9e2366ead1b45135f12ccd4674f8b907bf6fb2e84a6de6d3');

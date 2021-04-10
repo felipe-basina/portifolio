f8194fcf9cdddfdd48964d18063e214423a0985e
felipe-basina <felipe.basina@gmail.com>
2021-04-10 16:24:24 -0300

    Adicionado migracoes para tabela de contatos.

---
+++ atol/migrations/004_insert_sample_contacts.sql
@@ -0,0 +1,4 @@
+INSERT INTO t_contact (contact_name, email, phone_number, owner_idt)
+values ('Mary Pops', 'mary.pops@domain.net', '11998761234', 1);
+INSERT INTO t_contact (contact_name, email, phone_number, owner_idt)
+values ('John Doe', 'john.doe@domain.com',, '11912349876', 2);

3dc46775153c2e4851ade93884e18112989e71af
felipe-basina <felipe.basina@gmail.com>
2021-04-14 22:27:12 -0300

    Adicionado sql para recuperar registro por id e id_owner + atualizacao de registro de contato.

--- atol/resources/sql/contact-queries.sql
+++ atol/resources/sql/contact-queries.sql
@@ -9,3 +9,18 @@ VALUES (:contact_name, :email, :phone_number, :owner_idt)
 SELECT * FROM t_contact
 WHERE owner_idt = :owner_idt
 ORDER BY idt
+
+-- :name get-contact :? :1
+-- :doc retrieves contact record given it's idt
+SELECT * FROM t_contact
+WHERE idt = :idt
+AND owner_idt = :owner_idt
+
+-- :name update-contact! :! :n
+-- :doc updates an existing contact record
+update t_contact set
+    contact_name = :contact_name,
+    email = :email,
+    phone_number = :phone_number,
+    update_dt = current_timestamp
+WHERE idt = :idt

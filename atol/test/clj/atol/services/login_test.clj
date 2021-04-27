2ed444f473d27c10579f1b2dfc3e1a93b9548f4f
felipe-basina <felipe.basina@gmail.com>
2021-04-26 21:37:05 -0300

    Adicionado testes de unidade para validacao de senha em autenticacao.

--- atol/test/clj/atol/services/login_test.clj
+++ atol/test/clj/atol/services/login_test.clj
@@ -1,6 +1,9 @@
 (ns atol.services.login-test
   (:require [atol.services.login :as ls]
+            [buddy.hashers :as hashers]
             [clojure.test :refer :all]))
+
+(def fake-owner [{:owner_pass "bcrypt+sha512$e5b8f9b454c679868ee210e8073a6ae7$12$009edae5051511124d39b51f809fbc5eb1848b1f8ba5801a"}])
 
 (deftest test-valid-login?
   (testing "check whether is valid login"
@@ -16,3 +19,13 @@
       (is (not (nil? validation-errors)))
       (is (contains? validation-errors :username))
       (is (= (:username validation-errors) "must be a valid email")))))
+
+(deftest test-invalid-password
+  (testing "check invalid password"
+    (let [raw-pass "123"]
+      (is (not (ls/valid-pass? raw-pass fake-owner))))))
+
+(deftest test-valid-password
+  (testing "check valid password"
+    (let [raw-pass "1234"]
+      (is (ls/valid-pass? raw-pass fake-owner)))))

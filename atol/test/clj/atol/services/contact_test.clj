4b38d28fac08f922ec1e5ae5deb7f1903b16721b
felipe-basina <felipe.basina@gmail.com>
2021-04-29 20:02:03 -0300

    Adicionado teste de unidade para recuperar contatos por filtro.

--- atol/test/clj/atol/services/contact_test.clj
+++ atol/test/clj/atol/services/contact_test.clj
@@ -71,3 +71,11 @@
   (testing "check fail delete contact"
     (let [contact (assoc {} :next.jdbc/update-count 0)]
       (is (not (sc/deleted? contact))))))
+
+(deftest test-get-contacts-by-filter
+  (with-redefs [db/filter-contacts (fn [_] (conj [] contact))]
+    (let [found-contacts (sc/get-contacts-by-filter "Name" 1001)]
+      (is (not-empty found-contacts))
+      (is (= 1 (count found-contacts)))
+      (is (vector? found-contacts))
+      (is (= contact (first found-contacts))))))

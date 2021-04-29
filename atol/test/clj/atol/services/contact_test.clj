c3e295eb12909ef8845121259361cf8f5b755f19
felipe-basina <felipe.basina@gmail.com>
2021-04-29 19:33:43 -0300

    Adicionado testes de unidade para recuperar contato(s).

--- atol/test/clj/atol/services/contact_test.clj
+++ atol/test/clj/atol/services/contact_test.clj
@@ -26,3 +26,20 @@
   (testing "check whether is an invalid phone number"
     (let [contact (assoc contact :phone_number "111999")]
       (is (nil? (sc/valid-phone? contact))))))
+
+(deftest test-get-contacts-by-owner-id
+  (testing "check get contacts by owner id"
+    (let [contacts (conj [] contact)
+          owner-id 1001]
+      (with-redefs [db/get-contacts (fn [_] contacts)]
+        (let [all-contacts (sc/get-contacts-by-owner-id owner-id)]
+          (is (not-empty all-contacts))
+          (is (= 1 (count all-contacts)))
+          (is (= contact (first all-contacts))))))))
+
+(deftest test-get-contact-by-id
+  (testing "check get contact by id"
+    (with-redefs [db/get-contact (fn [_] contact)]
+      (let [found-contact (sc/get-contact-by-id 1 1001)]
+        (is (not-empty found-contact))
+        (is (= contact found-contact))))))

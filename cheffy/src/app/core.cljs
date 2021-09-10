a814587431837f17b2721a992f09d612043fb0c1
felipe-basina <felipe.basina@gmail.com>
2021-09-09 22:21:52 -0300

    Adicionado chamada ao componente para carregamento dos dados que estao em memoria para exibicao no navegador.

--- cheffy/src/app/core.cljs
+++ cheffy/src/app/core.cljs
@@ -1,5 +1,7 @@
 (ns app.core
   (:require [reagent.core :as r]
+            [re-frame.core :as rf]
+            [app.db]
             [app.theme :refer [cheffy-theme]]
     ;; This is a react component so it needs to be imported using ""
             ["@smooth-ui/core-sc" :refer [Normalize ThemeProvider Button]]))
@@ -13,6 +15,7 @@
 
 (defn ^:dev/after-load start
       []
+      (rf/dispatch-sync [:initialize-db])                   ;; It runs async, so to guarantee all data will be available before rendering we need to force to be sync
       (r/render [app]
                 (.getElementById js/document "app")))
 

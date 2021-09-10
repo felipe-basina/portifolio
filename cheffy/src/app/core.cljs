953cf4694958a85152449da978e673267767edf8
felipe-basina <felipe.basina@gmail.com>
2021-09-09 22:03:39 -0300

    Adicionado configuracoes para definir layout atraves do smooth-ui.

--- cheffy/src/app/core.cljs
+++ cheffy/src/app/core.cljs
@@ -1,15 +1,21 @@
 (ns app.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [app.theme :refer [cheffy-theme]]
+    ;; This is a react component so it needs to be imported using ""
+            ["@smooth-ui/core-sc" :refer [Normalize ThemeProvider Button]]))
 
 (defn app
-  []
-  [:div "Cheffy"])
+      []
+      [:<>                                                  ;; This is a react 'fragment' which allows to return more than 1 component | It can be replaced by 'div'
+       [:> Normalize]                                       ;; Same as (r/adapt-react-class Normalize) | (r/adapt-react-class Button)
+       [:> ThemeProvider {:theme cheffy-theme}
+        [:> Button "Hello"]]])
 
 (defn ^:dev/after-load start
-  []
-  (r/render [app]
-    (.getElementById js/document "app")))
+      []
+      (r/render [app]
+                (.getElementById js/document "app")))
 
 (defn ^:export init
-  []
-  (start))
+      []
+      (start))

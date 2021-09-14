67956018bd3da52fe80692e76d8cd8dec4a813e6
felipe-basina <felipe.basina@gmail.com>
2021-09-13 23:01:39 -0300

    Adicionado componente para navegacao no site.

--- cheffy/src/app/core.cljs
+++ cheffy/src/app/core.cljs
@@ -2,6 +2,7 @@
   (:require [reagent.core :as r]
             [re-frame.core :as rf]
             [app.db]
+            [app.nav.views.nav :refer [nav]]
             [app.theme :refer [cheffy-theme]]
     ;; This is a react component so it needs to be imported using ""
             ["@smooth-ui/core-sc" :refer [Normalize ThemeProvider Button]]))
@@ -11,7 +12,7 @@
       [:<>                                                  ;; This is a react 'fragment' which allows to return more than 1 component | It can be replaced by 'div'
        [:> Normalize]                                       ;; Same as (r/adapt-react-class Normalize) | (r/adapt-react-class Button)
        [:> ThemeProvider {:theme cheffy-theme}
-        [:> Button "Hello"]]])
+        [nav]]])
 
 (defn ^:dev/after-load start
       []

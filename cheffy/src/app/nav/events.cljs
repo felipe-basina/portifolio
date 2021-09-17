9faac3b0f3a52443cd7200eafabe916c520a2a78
felipe-basina <felipe.basina@gmail.com>
2021-09-16 22:31:35 -0300

    Definido o evento para alterar a pagina de navegacao atual.

--- cheffy/src/app/nav/events.cljs
+++ cheffy/src/app/nav/events.cljs
@@ -1,1 +1,7 @@
-(ns app.nav.events)
+(ns app.nav.events
+  (:require [re-frame.core :refer [reg-event-db]]))
+
+(reg-event-db
+  :set-active-nav                                           ;; (rf/dispatch [:set-active-nav :recipes])
+  (fn [db [_ active-nav]]
+      (assoc-in db [:nav :active-nav] active-nav)))

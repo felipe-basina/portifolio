a31ecb6df485ac71639b907f18de41117346b6f8
felipe-basina <felipe.basina@gmail.com>
2021-09-16 23:00:18 -0300

    Adicionado simbolo active-nav na desconstrucao de parametros e utilizacao do mesmo para definir efeito ao selecionar um item do menu de navegacao.

--- cheffy/src/app/nav/views/nav_item.cljs
+++ cheffy/src/app/nav/views/nav_item.cljs
@@ -2,11 +2,12 @@
   (:require ["@smooth-ui/core-sc" :refer [Box]]))
 
 (defn nav-item
-      [{:keys [id name href dispatch]}]
-      [:> Box {:key      id                                 ;; The syntax :> does not work with list comprehension, can be also substitute by (r/adapt-react-class Box) without the :div
-               :as       "a"                                ;; Alias to HTML anchor
-               :href     href
-               :on-click dispatch
-               :ml       3                                  ;; Margin left
-               :pb       10}                                ;; Padding bottom
+      [{:keys [id name href dispatch active-nav]}]
+      [:> Box {:key           id                            ;; The syntax :> does not work with list comprehension, can be also substitute by (r/adapt-react-class Box) without the :div
+               :as            "a"                           ;; Alias to HTML anchor
+               :href          href
+               :on-click      dispatch
+               :ml            3                             ;; Margin left
+               :pb            10                            ;; Padding bottom
+               :border-bottom (when (= active-nav id) "2px solid #102A43")}
        name])

8383a2476a56dceac33b34291c426b80b4f3d4e4
felipe-basina <felipe.basina@gmail.com>
2021-09-16 23:00:46 -0300

    Adicionado funcao para ouvir eventos de active-nav.

--- cheffy/src/app/nav/subs.cljs
+++ cheffy/src/app/nav/subs.cljs
@@ -1,1 +1,8 @@
-(ns app.nav.subs)
+(ns app.nav.subs
+  (:require [re-frame.core :refer [reg-sub]]))
+
+;; When the event related to this specific keyword was fired then the function bellow will act getting the value
+(reg-sub
+  :active-nav
+  (fn [db _]
+      (get-in db [:nav :active-nav])))

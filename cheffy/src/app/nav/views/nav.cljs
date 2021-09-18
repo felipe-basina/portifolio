8574590680a1eb489cef118c187b355dbba0dd1c
felipe-basina <felipe.basina@gmail.com>
2021-09-17 22:04:19 -0300

    Adicionado componente publico a ser exibido quando usuario nao estiver autenticado.

--- cheffy/src/app/nav/views/nav.cljs
+++ cheffy/src/app/nav/views/nav.cljs
@@ -1,5 +1,6 @@
 (ns app.nav.views.nav
-  (:require [app.nav.views.authenticated :refer [authenticated]]))
+  (:require [app.nav.views.authenticated :refer [authenticated]]
+            [app.nav.views.public :refer [public]]))
 
 (defn nav
       "Redirects to the properly view accordingly to the authentication process"
@@ -7,4 +8,4 @@
       (let [user true]
            (if user
              authenticated
-             "public")))
+             public)))

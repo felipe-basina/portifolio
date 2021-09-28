665ce5ec92becb63b7be94a1a3b67da14d6c0d6a
felipe-basina <felipe.basina@gmail.com>
2021-09-27 22:50:02 -0300

    Adicionado eventos para definir alteracao de rotas e navegacao.

--- cheffy/src/app/nav/events.cljs
+++ cheffy/src/app/nav/events.cljs
@@ -1,7 +1,18 @@
 (ns app.nav.events
-  (:require [re-frame.core :refer [reg-event-db]]))
+  (:require [re-frame.core :refer [reg-event-db reg-fx]]
+            [app.router :as router]))
 
 (reg-event-db
   :set-active-nav                                           ;; (rf/dispatch [:set-active-nav :recipes])
   (fn [db [_ active-nav]]
       (assoc-in db [:nav :active-nav] active-nav)))
+
+(reg-event-db
+  :route-changed
+  (fn [db [_ {:keys [handler]}]]
+      (assoc-in db [:nav :active-page] handler)))
+
+(reg-fx
+  :navigate-to                                              ;; (:navigate-to {:path "/saved})
+  (fn [{:keys [path]}]
+      (router/set-token! path)))

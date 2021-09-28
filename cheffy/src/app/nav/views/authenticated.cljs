89267caf535104b98b0657fb3212034a4122d02e
felipe-basina <felipe.basina@gmail.com>
2021-09-27 22:16:11 -0300

    Refatoracao para utilizar funcao para disparar evento relacionado ao item do menu selecionado.

--- cheffy/src/app/nav/views/authenticated.cljs
+++ cheffy/src/app/nav/views/authenticated.cljs
@@ -1,5 +1,6 @@
 (ns app.nav.views.authenticated
   (:require [re-frame.core :as rf]
+            [app.router :as router]
             [app.nav.views.nav-item :refer [nav-item]]
             ["@smooth-ui/core-sc" :refer [Box]]))
 
@@ -8,23 +9,23 @@
       (let [active-nav @(rf/subscribe [:active-nav])        ;; The return of rf/subscribe is an ATOM, so it is needed to deferred it to get the values
             nav-items [{:id       :saved
                         :name     "Saved"
-                        :href     "#saved"
+                        :href     (router/path-for :saved)
                         :dispatch #(rf/dispatch [:set-active-nav :saved])}
                        {:id       :recipes
                         :name     "Recipes"
-                        :href     "#recipes"
+                        :href     (router/path-for :recipes)
                         :dispatch #(rf/dispatch [:set-active-nav :recipes])}
                        {:id       :inboxes
                         :name     "Inbox"
-                        :href     "#inbox"
+                        :href     (router/path-for :inboxes)
                         :dispatch #(rf/dispatch [:set-active-nav :inboxes])}
                        {:id       :become-a-chef
                         :name     "Chef"
-                        :href     "#become-a-chef"
+                        :href     (router/path-for :become-a-chef)
                         :dispatch #(rf/dispatch [:set-active-nav :become-a-chef])}
                        {:id       :profile
                         :name     "Profile"
-                        :href     "#profile"
+                        :href     (router/path-for :profile)
                         :dispatch #(rf/dispatch [:set-active-nav :profile])}]]
            [:> Box {:display         "flex"
                     :justify-content "flex-end"

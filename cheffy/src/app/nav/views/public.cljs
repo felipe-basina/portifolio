96ffde2a2cc84b3e9bf61ea3eeb3213716446c25
felipe-basina <felipe.basina@gmail.com>
2021-09-30 22:20:17 -0300

    Refatoracao para utilizar active-page

--- cheffy/src/app/nav/views/public.cljs
+++ cheffy/src/app/nav/views/public.cljs
@@ -1,35 +1,36 @@
 (ns app.nav.views.public
   (:require [re-frame.core :as rf]
+            [app.router :as router]
             [app.nav.views.nav-item :refer [nav-item]]
             ["@smooth-ui/core-sc" :refer [Box]]))
 
 (defn public
       []
-      (let [active-nav @(rf/subscribe [:active-nav])        ;; The return of rf/subscribe is an ATOM, so it is needed to deferred it to get the values
+      (let [active-page @(rf/subscribe [:active-page])      ;; The return of rf/subscribe is an ATOM, so it is needed to deferred it to get the values
             nav-items [{:id       :recipes
                         :name     "Recipes"
-                        :href     "#recipes"
-                        :dispatch #(rf/dispatch [:set-active-nav :recipes])}
+                        :href     (router/path-for :recipes)
+                        :dispatch #(rf/dispatch [:set-active-page :recipes])}
                        {:id       :become-a-chef
                         :name     "Chef"
-                        :href     "#become-a-chef"
-                        :dispatch #(rf/dispatch [:set-active-nav :become-a-chef])}
+                        :href     (router/path-for :become-a-chef)
+                        :dispatch #(rf/dispatch [:set-active-page :become-a-chef])}
                        {:id       :sign-up
                         :name     "Sign up"
-                        :href     "#sign-up"
-                        :dispatch #(rf/dispatch [:set-active-nav :sign-up])}
+                        :href     (router/path-for :sign-up)
+                        :dispatch #(rf/dispatch [:set-active-page :sign-up])}
                        {:id       :log-in
                         :name     "Login"
-                        :href     "#log-in"
+                        :href     (router/path-for :log-in)
                         :dispatch #(rf/dispatch [:set-active-nav :log-in])}]]
            [:> Box {:display         "flex"
                     :justify-content "flex-end"
                     :py              1}
             (for [{:keys [id name href dispatch]} nav-items]
                  ;; ^{:key (:id item)}                         ;; Added metadata to set the unique id of the component
-                 [nav-item {:key        id
-                            :id         id
-                            :name       name
-                            :href       href
-                            :dispatch   dispatch
-                            :active-nav active-nav}])]))
+                 [nav-item {:key         id
+                            :id          id
+                            :name        name
+                            :href        href
+                            :dispatch    dispatch
+                            :active-page active-page}])]))

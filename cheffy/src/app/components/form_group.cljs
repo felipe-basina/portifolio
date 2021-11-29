9e86c1c004504e45b25af812cf78a305d9d86fe3
felipe-basina <felipe.basina@gmail.com>
2021-11-29 20:51:49 -0300

    Adicionado componente para area de texto; mapeado atributo on-key-down.

--- cheffy/src/app/components/form_group.cljs
+++ cheffy/src/app/components/form_group.cljs
@@ -2,14 +2,22 @@
   (:require [reagent.core :as r]
             [re-frame.core :as rf]
             [app.components.page-nav :refer [page-nav]]
-            ["@smooth-ui/core-sc" :refer [FormGroup Label Input]]))
+            ["@smooth-ui/core-sc" :refer [FormGroup Label Input TextArea]]))
 
 (defn form-group
-      [{:keys [id label type values]}]
+      [{:keys [id label type values textarea on-key-down]}]
       [:> FormGroup
        [:> Label {:html-for id} label]
-       [:> Input {:control   true
-                  :id        id
-                  :type      type
-                  :value     (id @values)
-                  :on-change #(swap! values assoc id (.. % -target -value))}]])
+       (if textarea
+         [:> TextArea {:control   true
+                       :rows      6
+                       :id        id
+                       :type      type
+                       :value     (id @values)
+                       :on-change #(swap! values assoc id (.. % -target -value))}]
+         [:> Input {:control     true
+                    :id          id
+                    :type        type
+                    :value       (id @values)
+                    :on-change   #(swap! values assoc id (.. % -target -value))
+                    :on-key-down on-key-down}])])

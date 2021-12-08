d74dd5970a1482c082f8f8cc98f3a0996eed9e23
felipe-basina <felipe.basina@gmail.com>
2021-12-08 11:59:18 -0300

    Refatoracao para permitir melhor distribuicao de eventos a partir de eventos base.

--- cheffy/src/app/nav/subs.cljs
+++ cheffy/src/app/nav/subs.cljs
@@ -1,18 +1,39 @@
 (ns app.nav.subs
-  (:require [re-frame.core :refer [reg-sub]]))
+  (:require [re-frame.core :refer [reg-sub subscribe]]))
+
+(reg-sub
+  :nav
+  (fn [db _]
+      (get db :nav)))
 
 ;; When the event related to this specific keyword was fired then the function bellow will act getting the value
 (reg-sub
   :active-nav
-  (fn [db _]
-      (get-in db [:nav :active-nav])))
+  ;(fn [_] (subscribe [:nav]))                               ;; another to do this is as following below
+  :<- [:nav]
+  (fn [nav _]
+      (get nav :active-nav)))
 
 (reg-sub
   :active-page
-  (fn [db _]
-      (get-in db [:nav :active-page])))
+  (fn [_] (subscribe [:nav]))
+  (fn [nav _]
+      (get nav :active-page)))
 
 (reg-sub
   :active-modal
-  (fn [db _]
-      (get-in db [:nav :active-modal])))
+  (fn [_] (subscribe [:nav]))
+  (fn [nav _]
+      (get nav :active-modal)))
+
+(reg-sub
+  :active-inbox
+  :<- [:nav]
+  (fn [nav _]
+      (:active-inbox nav)))
+
+(reg-sub
+  :active-recipe
+  :<- [:nav]
+  (fn [nav _]
+      (:active-recipe nav)))

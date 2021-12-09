c6ba9fc1d735f4e73601eb2beb051acf6ffbdfad
felipe-basina <felipe.basina@gmail.com>
2021-12-08 21:25:51 -0300

    Refatoracao para utilizar tracing nas funcoes e assim visualizar os eventos executados para debugging da aplicacao.

--- cheffy/src/app/errors/events.cljs
+++ cheffy/src/app/errors/events.cljs
@@ -1,12 +1,13 @@
 (ns app.errors.events
-  (:require [re-frame.core :refer [reg-event-db]]))
+  (:require [re-frame.core :refer [reg-event-db]]
+            [day8.re-frame.tracing :refer-macros [fn-traced]]))
 
 (reg-event-db
   :has-value?
-  (fn [db [_ id]]
-      (assoc-in db [:errors id] "Can't be blank")))
+  (fn-traced [db [_ id]]
+             (assoc-in db [:errors id] "Can't be blank")))
 
 (reg-event-db
   :clear-error
-  (fn [db [_ id]]
-      (update-in db [:errors] dissoc id)))
+  (fn-traced [db [_ id]]
+             (update-in db [:errors] dissoc id)))

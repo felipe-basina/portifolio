4e815ab235a3e7aff9b7f874934735f00e7303f4
felipe-basina <felipe.basina@gmail.com>
2021-12-08 21:58:05 -0300

    Refatoracao dos componentes de textos para evitar o problema do cursor sempre ir para o final da palavra.

--- cheffy/src/app/components/form_group.cljs
+++ cheffy/src/app/components/form_group.cljs
@@ -3,13 +3,29 @@
             [re-frame.core :as rf]
             [clojure.string :as str]
             [app.components.page-nav :refer [page-nav]]
-            ["@smooth-ui/core-sc" :refer [FormGroup Label Input TextArea ControlFeedback]]))
+            ["@smooth-ui/core-sc" :refer [FormGroup Label ControlFeedback Input Textarea]]))
+
+;; Removing some attributes to avoid collision
+(def skip [:__scTheme :theme :control :valid])
+
+;; A reagent input: converts the values from input into a reagent input
+(def r-input
+  (r/reactify-component
+    (fn [props]
+        [:input (apply dissoc props skip)])))               ;; Apply the function `dissoc` to remove values in `skip` from `props`
+
+(def r-textarea
+  (r/reactify-component
+    (fn [props]
+        [:textarea (apply dissoc props skip)])))
 
 (defn form-group
-      [{:keys [id label type values textarea on-key-down]}]
+      [{:keys [id label type values element on-key-down] :or {element Input}}] ;; If `element` was not provided so set it as Input
       (let [errors @(rf/subscribe [:errors])
             input-error (get errors id)
             is-empty? (str/blank? (id @values))
+            textarea (= element Textarea)
+            input (= element Input)
             valid (if input-error false nil)
             validate (fn []
                          (if is-empty?
@@ -17,23 +33,37 @@
                            (rf/dispatch [:clear-error id])))]
            [:> FormGroup
             [:> Label {:html-for id} label]
-            (if textarea
-              [:> TextArea {:control   true
-                            :valid     valid
-                            :on-blur   validate
-                            :rows      6
-                            :id        id
-                            :type      type
-                            :value     (id @values)
-                            :on-change #(swap! values assoc id (.. % -target -value))}]
-              [:> Input {:control     true
+            [:> element {:as          (cond
+                                        input r-input
+                                        textarea r-textarea)
+                         :control     true
                          :valid       valid
                          :on-blur     validate
+                         :rows        6
                          :id          id
                          :type        type
                          :value       (id @values)
                          :on-change   #(swap! values assoc id (.. % -target -value))
-                         :on-key-down on-key-down}])
+                         :on-key-down on-key-down}]
+            (comment                                        ;; Before refactoring
+              (if textarea
+                [:> TextArea {:control     true
+                              :valid       valid
+                              :on-blur     validate
+                              :rows        (when (= element Textarea) 6)
+                              :id          id
+                              :type        type
+                              :value       (id @values)
+                              :on-change   #(swap! values assoc id (.. % -target -value))
+                              :on-key-down on-key-down}]
+                [:> Input {:control     true
+                           :valid       valid
+                           :on-blur     validate
+                           :id          id
+                           :type        type
+                           :value       (id @values)
+                           :on-change   #(swap! values assoc id (.. % -target -value))
+                           :on-key-down on-key-down}]))
             (when input-error
                   [:> ControlFeedback {:valid false}
                    input-error])]))

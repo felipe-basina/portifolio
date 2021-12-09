1cf2f2f6dca8b636d6f09b438c3119eb4984a9c3
felipe-basina <felipe.basina@gmail.com>
2021-12-08 21:26:00 -0300

    Refatoracao para utilizar tracing nas funcoes e assim visualizar os eventos executados para debugging da aplicacao.

--- cheffy/src/app/nav/events.cljs
+++ cheffy/src/app/nav/events.cljs
@@ -1,54 +1,55 @@
 (ns app.nav.events
   (:require [re-frame.core :refer [reg-event-db reg-event-fx reg-fx path]]
-            [app.router :as router]))
+            [app.router :as router]
+            [day8.re-frame.tracing :refer-macros [fn-traced]]))
 
 (def nav-interceptors [(path :nav)])                        ;; Specifies which node from the db should be returned
 
 (reg-event-db
   :set-active-nav                                           ;; (rf/dispatch [:set-active-nav :recipes])
   nav-interceptors
-  (fn [nav [_ active-nav]]
-      (assoc nav :active-nav active-nav)))
+  (fn-traced [nav [_ active-nav]]
+             (assoc nav :active-nav active-nav)))
 
 (reg-event-db
   :set-active-page
   nav-interceptors
-  (fn [nav [_ active-page]]
-      (assoc nav :active-page active-page)))
+  (fn-traced [nav [_ active-page]]
+             (assoc nav :active-page active-page)))
 
 (reg-event-fx
   :route-changed
   nav-interceptors
-  (fn [{:keys [db]} [_ {:keys [handler route-params]}]]
-      (let [nav db
-            nav (assoc nav :active-page handler)]
-           (case handler
-                 :recipes
-                 {:db       nav
-                  :dispatch [:get-recipes]}
+  (fn-traced [{:keys [db]} [_ {:keys [handler route-params]}]]
+             (let [nav db
+                   nav (assoc nav :active-page handler)]
+                  (case handler
+                        :recipes
+                        {:db       nav
+                         :dispatch [:get-recipes]}
 
-                 :recipe
-                 {:db       (assoc nav :active-recipe (keyword (:recipe-id route-params)))
-                  :dispatch [:get-recipes]}
+                        :recipe
+                        {:db       (assoc nav :active-recipe (keyword (:recipe-id route-params)))
+                         :dispatch [:get-recipes]}
 
-                 :inbox
-                 {:db (assoc nav :active-inbox (keyword (:inbox-id route-params)))}
+                        :inbox
+                        {:db (assoc nav :active-inbox (keyword (:inbox-id route-params)))}
 
-                 {:db (dissoc nav :active-recipe :active-inbox)})))) ;; Default case
+                        {:db (dissoc nav :active-recipe :active-inbox)})))) ;; Default case
 
 (reg-fx
   :navigate-to                                              ;; (:navigate-to {:path "/saved})
-  (fn [{:keys [path]}]
-      (router/set-token! path)))
+  (fn-traced [{:keys [path]}]
+             (router/set-token! path)))
 
 (reg-event-db
   :close-modal
   nav-interceptors
-  (fn [nav _]
-      (assoc nav :active-modal nil)))
+  (fn-traced [nav _]
+             (assoc nav :active-modal nil)))
 
 (reg-event-db
   :open-modal
   nav-interceptors
-  (fn [nav [_ modal-name]]
-      (assoc nav :active-modal modal-name)))
+  (fn-traced [nav [_ modal-name]]
+             (assoc nav :active-modal modal-name)))

12e2dd72a167faede19eb914e38d5af42d8d8e0b
felipe-basina <felipe.basina@gmail.com>
2021-12-09 21:52:36 -0300

    Adicionado o interceptador, ao iniciar o carregamento das informacoes de db, para validacao de conteudo.

--- cheffy/src/app/db.cljs
+++ cheffy/src/app/db.cljs
@@ -1,5 +1,6 @@
 (ns app.db
-  (:require [re-frame.core :as rf]))
+  (:require [re-frame.core :as rf]
+            [app.spec :refer [check-spec-interceptor]]))
 
 ;; Before using the API to fetch recipes
 (comment
@@ -370,6 +371,7 @@
 
 (rf/reg-event-fx
   :initialize-db
-  [(rf/inject-cofx :local-store-user)]
+  [(rf/inject-cofx :local-store-user)
+   check-spec-interceptor]
   (fn [{:keys [local-store-user]} _]
       {:db (assoc-in initial-app-db [:auth] local-store-user)}))

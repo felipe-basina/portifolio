ff919b44848e5488c698bc43a2a2229d8f833a4b
felipe-basina <felipe.basina@gmail.com>
2021-12-09 21:53:04 -0300

    Ajustes na definicao das regras para validacao.

--- cheffy/src/app/spec.cljs
+++ cheffy/src/app/spec.cljs
@@ -7,19 +7,19 @@
       (when-not (s/valid? a-spec db)
                 (throw (ex-info (str "spec check failed: " (s/explain-str a-spec db)) {}))))
 
-(s/def app.spec/uid (s/nilable string?))
-(s/def ::auth (s/map-of keyword app.spec/uid))
+(s/def ::uid (s/nilable string?))
+(s/def ::auth (s/map-of keyword? ::uid))
 
 (s/def ::errors (s/map-of keyword? string?))
 
 (s/def ::inboxes (s/map-of keyword? map?))
 
-(s/def ::nav (s/map-of keyword? keyword?))
+(s/def ::nav (s/map-of keyword? (s/nilable keyword?)))
 
 (s/def ::recipes (s/map-of keyword? map?))
 
-(s/def ::users (s/map-of keyword? map?))
+(s/def ::users (s/map-of string? map?))
 
-(s/def ::db (s/keys :req-un [::auth :errors ::inboxes ::nav ::recipes ::users]))
+(s/def ::db (s/keys :req-un [::auth ::errors ::inboxes ::nav ::recipes ::users]))
 
 (def check-spec-interceptor (rf/after (partial check-and-throw ::db)))

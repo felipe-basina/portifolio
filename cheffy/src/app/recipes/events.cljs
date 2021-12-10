a5b9a2fe1ca75f5438f69e8998ca1d53fe84d6d5
felipe-basina <felipe.basina@gmail.com>
2021-12-09 22:12:52 -0300

    Refatoracao para remover codigos comentados; adicionado interceptador para validacao de formato de valores.

--- cheffy/src/app/recipes/events.cljs
+++ cheffy/src/app/recipes/events.cljs
@@ -5,7 +5,10 @@
             [day8.re-frame.tracing :refer-macros [fn-traced]]
             [ajax.core :as ajax]
             [clojure.walk :as w]
-            [app.helpers :as h]))
+            [app.helpers :as h]
+            [app.spec :refer [check-spec-interceptor]]))
+
+(def recipes-interceptors [check-spec-interceptor])
 
 (def recipes-endpoint "https://gist.githubusercontent.com/jacekschae/50ffe6e8851a5dfe35e932682ca32d85/raw/06e8041d0abf86e2c5d809a334cf8f18d3d6303b/recipes.json")
 
@@ -20,14 +23,7 @@
 
 (reg-event-fx
   :get-recipes
-  (comment
-    (fn [{:keys [db]} _]
-        {:db         (assoc-in db [:loading :recipes] true)
-         :http-xhrio {:method          :get
-                      :uri             recipes-endpoint
-                      :response-format (ajax/json-response-format {:keyword? true})
-                      :on-success      [:get-recipes-success]
-                      :on-failure      [:endpoint-request-error :get-recipes]}}))
+  recipes-interceptors
   (fn-traced [{:keys [db]} _]
              {:db         (assoc-in db [:loading :recipes] true)
               :http-xhrio {:method          :get
@@ -38,11 +34,6 @@
 
 (reg-event-db
   :get-recipes-success
-  (comment
-    (fn [db [_ recipes]]
-        (-> db
-            (assoc-in [:loading :recipes] false)
-            (assoc-in [:recipes] (keywordize-id recipes)))))
   (fn-traced [db [_ recipes]]
              (-> db
                  (assoc-in [:loading :recipes] false)
@@ -50,11 +41,6 @@
 
 (reg-event-db
   :endpoint-request-error
-  (comment
-    (fn [db [_ request-type response]]
-        (-> db
-            (assoc-in [:errors request-type] (get response :status-text))
-            (assoc-in [:loading request-type] false))))
   (fn-traced [db [_ request-type response]]
              (-> db
                  (assoc-in [:errors request-type] (get response :status-text))
@@ -62,12 +48,7 @@
 
 (reg-event-db
   :save-recipe
-  (comment
-    (fn [db [_ recipe-id]]
-        (let [uid (get-in db [:auth :uid])]
-             (-> db
-                 (update-in [:users uid :saved] conj recipe-id)
-                 (update-in [:recipes recipe-id :saved-count] inc)))))
+  recipes-interceptors
   (fn-traced [db [_ recipe-id]]
              (let [uid (get-in db [:auth :uid])]
                   (-> db
@@ -76,11 +57,7 @@
 
 (reg-event-db
   :delete-ingredient
-  (comment
-    (fn [{:keys [db]} [_ ingredient-id]]
-        (let [recipe-id (get-in db [:nav :active-recipe])]
-             {:db       (update-in db [:recipes recipe-id :ingredients] dissoc ingredient-id)
-              :dispatch [:close-modal]})))
+  recipes-interceptors
   (fn-traced [db [_ ingredient-id]]
              (let [recipe-id (get-in db [:nav :active-recipe])]
                   (-> db
@@ -89,18 +66,7 @@
 
 (reg-event-db
   :upsert-ingredient
-  (comment
-    (fn [{:keys [db]} [_ {:keys [id name amount measure]}]]
-        (let [recipe-id (get-in db [:nav :active-recipe])
-              ingredients (get-in db [:recipes recipe-id :ingredients])
-              order (or (get-in ingredients [id :order])
-                        (inc (count ingredients)))]
-             {:db       (assoc-in db [:recipes recipe-id :ingredients id] {:id      id
-                                                                           :order   order
-                                                                           :name    name
-                                                                           :amount  amount
-                                                                           :measure measure})
-              :dispatch [:close-modal]})))
+  recipes-interceptors
   (fn-traced [db [_ {:keys [id name amount measure]}]]
              (let [recipe-id (get-in db [:nav :active-recipe])
                    ingredients (get-in db [:recipes recipe-id :ingredients])
@@ -116,11 +82,7 @@
 
 (reg-event-db
   :delete-step
-  (comment
-    (fn [{:keys [db]} [_ step-id]]
-        (let [recipe-id (get-in db [:nav :active-recipe])]
-             {:db       (update-in db [:recipes recipe-id :steps] dissoc step-id)
-              :dispatch [:close-modal]})))
+  recipes-interceptors
   (fn-traced [db [_ step-id]]
              (let [recipe-id (get-in db [:nav :active-recipe])]
                   (-> db
@@ -129,16 +91,7 @@
 
 (reg-event-db
   :upsert-step
-  (comment
-    (fn [{:keys [db]} [_ {:keys [id desc]}]]
-        (let [recipe-id (get-in db [:nav :active-recipe])
-              steps (get-in db [:recipes recipe-id :steps])
-              order (or (get-in steps [id :order])
-                        (inc (count steps)))]
-             {:db       (assoc-in db [:recipes recipe-id :steps id] {:id    id
-                                                                     :order order
-                                                                     :desc  desc})
-              :dispatch [:close-modal]})))
+  recipes-interceptors
   (fn-traced [db [_ {:keys [id desc]}]]
              (let [recipe-id (get-in db [:nav :active-recipe])
                    steps (get-in db [:recipes recipe-id :steps])
@@ -152,17 +105,7 @@
 
 (reg-event-db
   :upsert-recipe
-  (comment
-    (fn [{:keys [db]} [_ {:keys [name prep-time]}]]
-        (let [recipe-id (get-in db [:nav :active-recipe])
-              id (or recipe-id (keyword (str "recipe-" (random-uuid))))
-              uid (get-in db [:auth :uid])]
-             {:db       (update-in db [:recipes id] merge {:id        id
-                                                           :name      name
-                                                           :prep-time prep-time
-                                                           :cook      uid
-                                                           :public?   false})
-              :dispatch [:close-modal]})))
+  recipes-interceptors
   (fn-traced [db [_ {:keys [name prep-time]}]]
              (let [recipe-id (get-in db [:nav :active-recipe])
                    id (or recipe-id (keyword (str "recipe-" (random-uuid))))
@@ -178,14 +121,7 @@
 ;; Dispatches "n" events, then redirect the user to route /recipes/
 (reg-event-fx
   :delete-recipe
-  (comment
-    (fn [{:keys [db]} _]
-        (let [recipe-id (get-in db [:nav :active-recipe])]
-             {:db          (update-in db [:recipes] dissoc recipe-id)
-              :dispatch-n  [[:set-active-page :recipes]
-                            [:set-active-nav :recipes]
-                            [:close-modal]]
-              :navigate-to {:path "/recipes/"}})))
+  recipes-interceptors
   (fn-traced [{:keys [db]} _]
              (let [recipe-id (get-in db [:nav :active-recipe])]
                   {:db          (update-in db [:recipes] dissoc recipe-id)
@@ -196,12 +132,7 @@
 
 (reg-event-db
   :publish-recipe
-  (comment
-    (fn [{:keys [db]} [_ {:keys [price]}]]
-        (let [recipe-id (get-in db [:nav :active-recipe])]
-             {:db       (update-in db [:recipes recipe-id] merge {:price   price
-                                                                  :public? true})
-              :dispatch [:close-modal]})))
+  recipes-interceptors
   (fn-traced [db [_ {:keys [price]}]]
              (let [recipe-id (get-in db [:nav :active-recipe])]
                   (-> db
@@ -211,11 +142,7 @@
 
 (reg-event-db
   :unpublish-recipe
-  (comment
-    (fn [{:keys [db]} _]
-        (let [recipe-id (get-in db [:nav :active-recipe])]
-             {:db       (assoc-in db [:recipes recipe-id :public?] false)
-              :dispatch [:close-modal]})))
+  recipes-interceptors
   (fn-traced [db _]
              (let [recipe-id (get-in db [:nav :active-recipe])]
                   (-> db
@@ -224,11 +151,7 @@
 
 (reg-event-db
   :upsert-image
-  (comment
-    (fn [{:keys [db]} [_ {:keys [img]}]]
-        (let [recipe-id (get-in db [:nav :active-recipe])]
-             {:db       (assoc-in db [:recipes recipe-id :img] img)
-              :dispatch [:close-modal]})))
+  recipes-interceptors
   (fn-traced [db [_ {:keys [img]}]]
              (let [recipe-id (get-in db [:nav :active-recipe])]
                   (-> db

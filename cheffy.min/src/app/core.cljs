2240a9ebd966d9122e2c11cce628e48c618f0731
felipe-basina <felipe.basina@gmail.com>
2021-12-22 19:32:02 -0300

    Adicionado componentes para estilizacao do projeto.

--- cheffy.min/src/app/core.cljs
+++ cheffy.min/src/app/core.cljs
@@ -1,15 +1,21 @@
 (ns app.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [app.theme :refer [cheffy-theme]]
+            ["@smooth-ui/core-sc" :refer [Normalize Button ThemeProvider]]))
 
 (defn app
-  []
-  [:div "Cheffy"])
+      []
+      [:<>
+       [:> Normalize]                                       ; Alternative to (r/adapt-react-class Normalize)
+       [:> ThemeProvider {:theme cheffy-theme}
+        [:> Button {:variant "danger"} "Hello"]
+        [:div "Cheffy"]]])
 
 (defn ^:dev/after-load start
-  []
-  (r/render [app]
-    (.getElementById js/document "app")))
+      []
+      (r/render [app]
+                (.getElementById js/document "app")))
 
 (defn ^:export init
-  []
-  (start))
+      []
+      (start))

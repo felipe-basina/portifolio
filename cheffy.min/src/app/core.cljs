d2d0b49ecdfa8dd6c290e8ca547c5a245c81b3e0
felipe-basina <felipe.basina@gmail.com>
2021-12-22 21:14:14 -0300

    Adicionado importacao do componente de listener de eventos de navegacao para ser carregado ao iniciar a aplicacao.

--- cheffy.min/src/app/core.cljs
+++ cheffy.min/src/app/core.cljs
@@ -5,6 +5,7 @@
     ;; -- nav --
             [app.nav.views.nav :refer [nav]]
             [app.nav.events]
+            [app.nav.subs]
             [app.theme :refer [cheffy-theme]]
             ["@smooth-ui/core-sc" :refer [Normalize Button ThemeProvider]]))
 

bfc19bcfed3fa297406b87cd78ba383e56def826
felipe-basina <felipe.basina@gmail.com>
2021-12-22 21:14:46 -0300

    Refatoracao para adicionar estilo no item de navegacao corrente.

--- cheffy.min/src/app/nav/views/nav_item.cljs
+++ cheffy.min/src/app/nav/views/nav_item.cljs
@@ -2,11 +2,12 @@
   (:require ["@smooth-ui/core-sc" :refer [Box]]))
 
 (defn nav-item
-      [{:keys [id name href dispatch]}]
-      [:> Box {:key      id
-               :as       "a"
-               :href     href
-               :on-click dispatch
-               :ml       2
-               :pb       10}
+      [{:keys [id name href dispatch active-nav]}]
+      [:> Box {:key           id
+               :as            "a"
+               :href          href
+               :on-click      dispatch
+               :ml            2
+               :pb            10
+               :border-bottom (when (= active-nav id) "2px solid #102A43")}
        name])

02f22031b14e4938e12d9a881b30db0d6a66a698
felipe-basina <felipe.basina@gmail.com>
2022-01-11 21:36:04 -0300

    Adicionado funcao para inicializar servidor mais handler para rotas http.

--- cheffy.api/src/cheffy/server.clj
+++ cheffy.api/src/cheffy/server.clj
@@ -1,5 +1,19 @@
 (ns cheffy.server
-  (:gen-class))
+  (:require [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty]))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/"
+        {:get {:handler (fn [req] {:status 200
+                                   :body   "Hello Reitit"})}}]])))
+
+(defn start
+  []
+  (let [port 3000]
+    (jetty/run-jetty app {:port port :join? false})         ; join? false = do not block thread
+    (println (str "\n Server running on port " port))))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -7,4 +21,7 @@
   (println "Hello, World!"))
 
 (comment
+  (start)
+  (app {:request-method :get
+        :uri            "/"})
   (-main))

92b45b4427e855adfd443c38fef4a1493efbda9f
felipe-basina <felipe.basina@gmail.com>
2022-01-11 22:15:42 -0300

    Adicionado funcoes para inicializacao da aplicacao, utilizando as configuracoes.

--- cheffy.api/src/cheffy/server.clj
+++ cheffy.api/src/cheffy/server.clj
@@ -1,6 +1,8 @@
 (ns cheffy.server
   (:require [reitit.ring :as ring]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [integrant.core :as ig]
+            [environ.core :refer [env]]))
 
 (def app
   (ring/ring-handler
@@ -9,16 +11,44 @@
         {:get {:handler (fn [req] {:status 200
                                    :body   "Hello Reitit"})}}]])))
 
-(defn start
-  []
-  (let [port 3000]
-    (jetty/run-jetty app {:port port :join? false})         ; join? false = do not block thread
-    (println (str "\n Server running on port " port))))
+;------------------------------------------------------------------;
+; INIT                                                             ;
+; Settings to integrate the integrant component (app auto reload)  ;
+;------------------------------------------------------------------;
+(defmethod ig/prep-key :server/jetty
+  [_ config]
+  (merge config {:port (Integer/parseInt (env :port))}))
+
+(defmethod ig/init-key :server/jetty
+  [_ {:keys [handler port]}]                                ; Equivalent to {:server/jetty {:handler (ig/ref :cheffy/app) :port 3000}
+  (println (str "\nServer running on port " port))
+  (jetty/run-jetty handler {:port port :join? false}))      ; join? false = do not block thread
+
+(defmethod ig/init-key :cheffy/app
+  [_ config]
+  (println "\nStarted app")
+  (app config))
+
+(defmethod ig/init-key :db/postgres
+  [_ config]
+  (println "\nConfigured db")
+  (:jdbc-url config))
+
+(defmethod ig/halt-key! :server/jetty
+  [_ jetty]
+  (.stop jetty))
+;------------------------------------------------------------------;
+; END                                                              ;
+;------------------------------------------------------------------;
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [config-file]
+  (let [config (-> config-file
+                   slurp
+                   ig/read-string)]
+    (-> config
+        ig/prep
+        ig/init)))
 
 (comment
   (start)

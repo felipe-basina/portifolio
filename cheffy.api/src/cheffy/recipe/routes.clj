66d33920fec08702e483372c22245c2fcf31a047
felipe-basina <felipe.basina@gmail.com>
2022-01-25 09:03:41 -0300

    Mapeado atributo responses no retorno das apis.

--- cheffy.api/src/cheffy/recipe/routes.clj
+++ cheffy.api/src/cheffy/recipe/routes.clj
@@ -1,14 +1,17 @@
 (ns cheffy.recipe.routes
-  (:require [cheffy.recipe.handlers :as recipe]))
+  (:require [cheffy.recipe.handlers :as recipe]
+            [cheffy.responses :as responses]))
 
 (defn routes
   [env]                                                     ; jdbc-url
   (let [db (:jdbc-url env)]
     ["/recipes" {:swagger {:tags ["recipes"]}}
      [""
-      {:get {:handler (recipe/list-all-recipes db)
-             :summary "List all recipes"}}]
+      {:get {:handler   (recipe/list-all-recipes db)
+             :responses {200 {:body responses/recipes}}
+             :summary   "List all recipes"}}]
      ["/:recipe-id"
       {:get {:handler    (recipe/retrieve-recipe db)
-             :parameters {:path {:recipe-id int?}}
+             :parameters {:path {:recipe-id string?}}
+             :responses  {200 {:body responses/recipes}}
              :summary    "Retrieve recipe"}}]]))

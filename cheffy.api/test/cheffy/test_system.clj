476dd5a0240bb184ee604c4bca388160d20fef37
felipe-basina <felipe.basina@gmail.com>
2022-06-27 21:36:48 -0300

    Adicionado estrutura inicial para permitir testes de integracao.

---
+++ cheffy.api/test/cheffy/test_system.clj
@@ -0,0 +1,22 @@
+(ns cheffy.test-system
+  (:require [clojure.test :refer :all]
+            [integrant.repl.state :as state]
+            [muuntaja.core :as m]
+            [ring.mock.request :as mock]))
+
+(defn test-endpoint
+  ([method uri]
+   (test-endpoint method uri nil))
+  ([method uri opts]
+   (let [app (-> state/system :cheffy/app)
+         request (app (-> (mock/request method uri)
+                          (cond-> (:body opts) (mock/json-body (:body opts)))))]
+     (update request :body (partial m/decode "application/json"))))) ;; Equivalent to (fn [data] (m/decode "application/json" data))
+
+(comment
+  (let [request (test-endpoint :get "/v1/recipes")
+        decoded-request (m/decode-response-body request)]   ;; Convert the byte array from the response into a json format
+    (assoc request :body decoded-request))
+  (test-endpoint :post "/v1/recipes" {:img       "string"
+                                      :name      "my name"
+                                      :prep-time 30}))

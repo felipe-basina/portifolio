9ea56060c78b0188f25d536f0bc385ee6fa57fee
felipe-basina <felipe.basina@gmail.com>
2022-06-28 17:08:59 -0300

    Adicionado testes de integracao para componente de receitas.

---
+++ cheffy.api/test/cheffy/recipes_test.clj
@@ -0,0 +1,18 @@
+(ns cheffy.recipes-test
+  (:require [clojure.test :refer :all]
+            [cheffy.server :refer :all]
+            [cheffy.test-system :as ts]))
+
+(deftest recipes-tests
+  (testing "List recipes"
+    (testing "with auth -- public and drafts"
+      (let [{:keys [status body]} (ts/test-endpoint :get "/v1/recipes")]
+        (is (= 200 status))
+        (is (vector? (:public body)))
+        (is (vector? (:drafts body)))))
+
+    (testing "without -- public"
+      (let [{:keys [status body]} (ts/test-endpoint :get "/v1/recipes")]
+        (is (= 200 status))
+        (is (vector? (:public body)))
+        (is (nil? (:drafts body)))))))

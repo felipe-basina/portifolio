e109f9fd8560eb6bcf1caa8a70dbb3f0190fa567
felipe-basina <felipe.basina@gmail.com>
2022-06-29 17:26:09 -0300

    Adicionado parametro identificando necessidade de utilizacao de autorizacao.

--- cheffy.api/test/cheffy/recipes_test.clj
+++ cheffy.api/test/cheffy/recipes_test.clj
@@ -6,13 +6,13 @@
 (deftest recipes-tests
   (testing "List recipes"
     (testing "with auth -- public and drafts"
-      (let [{:keys [status body]} (ts/test-endpoint :get "/v1/recipes")]
+      (let [{:keys [status body]} (ts/test-endpoint :get "/v1/recipes" {:auth true})]
         (is (= 200 status))
         (is (vector? (:public body)))
         (is (vector? (:drafts body)))))
 
     (testing "without -- public"
-      (let [{:keys [status body]} (ts/test-endpoint :get "/v1/recipes")]
+      (let [{:keys [status body]} (ts/test-endpoint :get "/v1/recipes" {:auth false})]
         (is (= 200 status))
         (is (vector? (:public body)))
         (is (nil? (:drafts body)))))))

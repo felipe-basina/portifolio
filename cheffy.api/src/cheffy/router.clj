2b068f763bb943db7f8283f1ef8b90107d4a6448
felipe-basina <felipe.basina@gmail.com>
2022-07-01 18:25:04 -0300

    Adicionado proriedades para formatar excecao e validar o conteudo passado para handlers e assim garantir que sejam definidas funcoes ao inves de string.

--- cheffy.api/src/cheffy/router.clj
+++ cheffy.api/src/cheffy/router.clj
@@ -7,7 +7,9 @@
             [reitit.ring.middleware.muuntaja :as muuntaja]
             [reitit.coercion.spec :as coercion-spec]
             [reitit.ring.coercion :as coercion]
-            [reitit.ring.middleware.exception :as exception]))
+            [reitit.ring.middleware.exception :as exception]
+            [reitit.dev.pretty :as pretty]
+            [reitit.ring.spec :as rs]))
 
 (def swagger-docs
   ["/swagger.json"
@@ -23,13 +25,15 @@
 ;; All middleware defined below will be global
 ;;-----------------------------------------------
 (def router-config
-  {:data {:coercion   coercion-spec/coercion
-          :muuntaja   m/instance                            ;; This with the middleware below will allow content negotiation (conversion): e.g. from bytearray to json
-          :middleware [swagger/swagger-feature
-                       muuntaja/format-middleware
-                       exception/exception-middleware       ;; This will send http 400 when there is an issue with parameters
-                       coercion/coerce-request-middleware   ;; This will convert request parameters to the values defined in :parameters {:path {:recipe-id int?}} (recipe.routes)
-                       coercion/coerce-response-middleware]}}
+  {:validate  rs/validate                                   ; Checks if handlers in routes are valid functions and not strings
+   :exception pretty/exception
+   :data      {:coercion   coercion-spec/coercion
+               :muuntaja   m/instance                       ;; This with the middleware below will allow content negotiation (conversion): e.g. from bytearray to json
+               :middleware [swagger/swagger-feature
+                            muuntaja/format-middleware
+                            exception/exception-middleware  ;; This will send http 400 when there is an issue with parameters
+                            coercion/coerce-request-middleware ;; This will convert request parameters to the values defined in :parameters {:path {:recipe-id int?}} (recipe.routes)
+                            coercion/coerce-response-middleware]}}
   )
 
 (defn routes

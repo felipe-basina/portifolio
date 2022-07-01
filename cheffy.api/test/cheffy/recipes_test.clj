6e7952b625864e9035006faf1f836184690464e6
felipe-basina <felipe.basina@gmail.com>
2022-07-01 10:35:00 -0300

    Adicionado cenarios de testes de integracao para receitas.

--- cheffy.api/test/cheffy/recipes_test.clj
+++ cheffy.api/test/cheffy/recipes_test.clj
@@ -3,6 +3,14 @@
             [cheffy.server :refer :all]
             [cheffy.test-system :as ts]))
 
+(def recipe-id (atom nil))
+
+(def recipe {:img       ""
+             :prep-time 30
+             :name      "My Test Recipe"})
+
+(def update-recipe (assoc recipe :public true))
+
 (deftest recipes-tests
   (testing "List recipes"
     (testing "with auth -- public and drafts"
@@ -16,3 +24,27 @@
         (is (= 200 status))
         (is (vector? (:public body)))
         (is (nil? (:drafts body)))))))
+
+(deftest recipe-tests
+  (testing "Create recipe"
+    (let [{:keys [status body]} (ts/test-endpoint :post "/v1/recipes" {:auth true :body recipe})]
+      (reset! recipe-id (:recipe-id body))
+      (is (= status 201))
+      (is (not (empty? body)))))
+
+  (testing "Update recipe"
+    (let [{:keys [status body]} (ts/test-endpoint :put (str "/v1/recipes/" @recipe-id) {:auth true :body update-recipe})]
+      (is (= status 204))
+      (is (nil? body))))
+
+  (testing "Delete recipe"
+    (let [{:keys [status body]} (ts/test-endpoint :delete (str "/v1/recipes/" @recipe-id) {:auth true})]
+      (reset! recipe-id (:recipe-id body))
+      (is (= status 204))
+      (is (nil? body)))))
+
+(comment
+  (ts/test-endpoint :post "/v1/recipes" {:auth true :body recipe})
+  (ts/test-endpoint :put "/v1/recipes/8737fd6a-1a45-44ea-97ed-005d79fff89f" {:auth true :body update-recipe})
+  (ts/test-endpoint :delete "/v1/recipes/a3c5ec2d-e1a1-463f-aeb7-4fa6bb6e8ac6" {:auth true})
+  )

8f644c321a5ae86b58788e98b8f69fa085f22bd4
felipe-basina <felipe.basina@gmail.com>
2022-07-01 20:30:17 -0300

    Adicionado testes de integracao para operacoes de favoritar e desfavoritar receitas.

--- cheffy.api/test/cheffy/recipes_test.clj
+++ cheffy.api/test/cheffy/recipes_test.clj
@@ -1,7 +1,8 @@
 (ns cheffy.recipes-test
   (:require [clojure.test :refer :all]
             [cheffy.server :refer :all]
-            [cheffy.test-system :as ts]))
+            [cheffy.test-system :as ts]
+            [muuntaja.core :as m]))
 
 (use-fixtures :once ts/token-fixture)
 
@@ -39,6 +40,16 @@
       (is (= status 204))
       (is (nil? body))))
 
+  (testing "Favorite recipe"
+    (let [{:keys [status body]} (ts/test-endpoint :post (str "/v1/recipes/" @recipe-id "/favorite") {:auth true})]
+      (is (= status 201))
+      (is (nil? body))))
+
+  (testing "Unfavorite recipe"
+    (let [{:keys [status body]} (ts/test-endpoint :delete (str "/v1/recipes/" @recipe-id "/favorite") {:auth false})]
+      (is (= status 204))
+      (is (nil? body))))
+
   (testing "Delete recipe"
     (let [{:keys [status body]} (ts/test-endpoint :delete (str "/v1/recipes/" @recipe-id) {:auth true})]
       (reset! recipe-id (:recipe-id body))
@@ -46,7 +57,15 @@
       (is (nil? body)))))
 
 (comment
+  (ts/test-endpoint :get "/v1/recipes" {:auth false})
+
   (ts/test-endpoint :post "/v1/recipes" {:auth true :body recipe})
+  (let [{:keys [body]} (ts/test-endpoint :get "/v1/recipes/7ad6ae9d-e6b6-4142-8924-c98da78032ab" {:auth false})]
+    (:recipe body))
+
   (ts/test-endpoint :put "/v1/recipes/8737fd6a-1a45-44ea-97ed-005d79fff89f" {:auth true :body update-recipe})
   (ts/test-endpoint :delete "/v1/recipes/a3c5ec2d-e1a1-463f-aeb7-4fa6bb6e8ac6" {:auth true})
+
+  (ts/test-endpoint :post "/v1/recipes/7ad6ae9d-e6b6-4142-8924-c98da78032ab/favorite" {:auth true})
+  (ts/test-endpoint :delete "/v1/recipes/7ad6ae9d-e6b6-4142-8924-c98da78032ab/favorite" {:auth true})
   )

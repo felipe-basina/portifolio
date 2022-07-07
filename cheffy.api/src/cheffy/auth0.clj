01aa7e7ad51cbd591929c154afa7648c8be70cc1
felipe-basina <felipe.basina@gmail.com>
2022-07-07 12:40:00 -0300

    Adicionado funcao para recuperar identificador do perfil gerenciador de receitas.

--- cheffy.api/src/cheffy/auth0.clj
+++ cheffy.api/src/cheffy/auth0.clj
@@ -31,6 +31,29 @@
        (m/decode-response-body)
        :access_token))
 
+(defn get-role-id
+  [token]
+  (->> {:headers          {"Authorization" (str "Bearer " token)}
+        :throw-exceptions false                             ; To show errors in a body response fashion
+        :content-type     :json
+        :cookie-policy    :standard}
+       (http/get "https://dev-fansjs74.us.auth0.com/api/v2/roles")
+       (m/decode-response-body)
+       (filter #(= (:name %1) "manage-recipes"))
+       (reduce +)
+       :id))
+
 (comment
-  (get-management-token)
+  (let [token (get-management-token)
+        role-id (get-role-id token)
+        uid "auth0|62bcacf353cb0e073428f840"]
+    (->> {:headers          {"Authorization" (str "Bearer " token)}
+          :cookie-policy    :standard
+          :content-type     :json
+          :throw-exceptions false
+          #_#_:body             (m/encode
+                              "application/json"
+                              {:roles [role-id]})}
+         (http/get (str "https://dev-fansjs74.us.auth0.com/api/v2/users/" uid "/roles"))))
+
   (get-test-token))

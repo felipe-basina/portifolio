4b721889f0e608738bf076c3d8c205a06944913c
felipe-basina <felipe.basina@gmail.com>
2022-07-07 19:27:20 -0300

    Refatoracao de componentes de testes; separacao de funcoes de fixture nos respectivos namespaces; adicionado funcao para recuperar o id do perfil para gerenciar receitas.

--- cheffy.api/src/cheffy/auth0.clj
+++ cheffy.api/src/cheffy/auth0.clj
@@ -2,21 +2,6 @@
   (:require [clj-http.client :as http]
             [muuntaja.core :as m]))
 
-(defn get-test-token
-  []
-  (->> {:content-type  :json
-        :cookie-policy :standard
-        :body          (m/encode "application/json"
-                                 {:client_id  "gUSjqIkdJVSyCkBgAg3Nmxqq2c5U5Ark"
-                                  :audience   "https://dev-fansjs74.us.auth0.com/api/v2/"
-                                  :grant_type "password"
-                                  :username   "testing@cheffy.app"
-                                  :password   "Testing#00"
-                                  :scope      "openid profile email"})}
-       (http/post "https://dev-fansjs74.us.auth0.com/oauth/token")
-       (m/decode-response-body)
-       :access_token))
-
 (defn get-management-token
   []
   (->> {:throw-exceptions false                             ; To show errors in a body response fashion
@@ -44,6 +29,37 @@
        :id))
 
 (comment
+  (create-auth0-user {:connection "Username-Password-Authentication"
+                      :email      "account-tests@cheffy.app"
+                      :password   "s#m3R4nd0m-pass"})
+
+  (->> {:headers          {"Authorization" (str "Bearer " (get-management-token))}
+        :throw-exceptions false                             ; To show errors in a body response fashion
+        :content-type     :json
+        :cookie-policy    :standard}
+       (http/get "https://dev-fansjs74.us.auth0.com/api/v2/users/auth0|62c73facfecd320827fc8a58")
+       (m/decode-response-body))
+
+  (->> {:headers          {"Authorization" (str "Bearer " (get-management-token))}
+        :throw-exceptions false                             ; To show errors in a body response fashion
+        :content-type     :json
+        :cookie-policy    :standard}
+       (http/get "https://dev-fansjs74.us.auth0.com/api/v2/users")
+       (m/decode-response-body))
+
+  (->> {:headers          {"Authorization" (str "Bearer " (get-management-token))}
+        :throw-exceptions false                             ; To show errors in a body response fashion
+        :content-type     :json
+        :cookie-policy    :standard}
+       (http/delete "https://dev-fansjs74.us.auth0.com/api/v2/users/auth0|62c74318fecd320827fc8afb"))
+
+  (->> {:headers          {"Authorization" (str "Bearer " (get-management-token))}
+        :throw-exceptions false                             ; To show errors in a body response fashion
+        :content-type     :json
+        :cookie-policy    :standard}
+       (http/get "https://dev-fansjs74.us.auth0.com/api/v2/roles")
+       (m/decode-response-body))
+
   (let [token (get-management-token)
         role-id (get-role-id token)
         uid "auth0|62bcacf353cb0e073428f840"]
@@ -51,9 +67,11 @@
           :cookie-policy    :standard
           :content-type     :json
           :throw-exceptions false
-          #_#_:body             (m/encode
-                              "application/json"
-                              {:roles [role-id]})}
+          #_#_:body (m/encode
+                      "application/json"
+                      {:roles [role-id]})}
          (http/get (str "https://dev-fansjs74.us.auth0.com/api/v2/users/" uid "/roles"))))
 
-  (get-test-token))
+  (get-test-token "testing@cheffy.app")
+  (get-role-id "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9OdTltRmE3RzdpTTV5WGt5bG44byJ9.eyJpc3MiOiJodHRwczovL2Rldi1mYW5zanM3NC51cy5hdXRoMC5jb20vIiwic3ViIjoiM2Q1d3JhNHAyYWlzb054S0pMSUFTajZodVZKSkVJcDZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vZGV2LWZhbnNqczc0LnVzLmF1dGgwLmNvbS9hcGkvdjIvIiwiaWF0IjoxNjU3MjI5MTk1LCJleHAiOjE2NTczMTU1OTUsImF6cCI6IjNkNXdyYTRwMmFpc29OeEtKTElBU2o2aHVWSkpFSXA2Iiwic2NvcGUiOiJyZWFkOnVzZXJzIHVwZGF0ZTp1c2VycyBkZWxldGU6dXNlcnMgY3JlYXRlOnVzZXJzIHJlYWQ6cm9sZXMgY3JlYXRlOnJvbGVfbWVtYmVycyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.N9ythhD1vCppQtgb4mX8HdzBiA1kiHXIb3ncg8OfW1X99m2mcQSwhZS0bBNU7i2Q3ps_qpzlQenPF3ccRHFf5SR5d17uidO52jhdYu-lvPRYgD8EN_2QawKIrsZ8b2lz3fxUH4vEw59R7Y3s2dcfJM0rhIbY3axCiFjwO8AG8NkLBmfOnNz3K7Q2V5z4WIZ3ycej30vuiN6KgQPcF_d97qp66TD4e2dhAbcIzOY9rkJU1avM4g6mvwvkfsPvudOBbIYJ4VmwKdP3hEZMJ6G_uyhFXnirV3keRoVWqqdmvPhD_EIhJJTYdVfLAp11SAr1JBwoFD_0hufB3SH9GqMRdQ")
+  (get-test-token "account-tests@cheffy.app"))

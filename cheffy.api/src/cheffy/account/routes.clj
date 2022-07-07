ca5a436c98c81a69234a63e385f7219c744da233
felipe-basina <felipe.basina@gmail.com>
2022-07-07 11:07:24 -0300

    Adicionado rota para remover conta.

--- cheffy.api/src/cheffy/account/routes.clj
+++ cheffy.api/src/cheffy/account/routes.clj
@@ -6,8 +6,11 @@
   [env]
   (let [db (:jdbc-url env)]
     ["/accounts" {:swagger    {:tags ["accounts"]}
-                 :middleware [[mw/wrap-auth0]]}
+                  :middleware [[mw/wrap-auth0]]}
      [""
-      {:post {:handler   (account/create-account! db)
-              :responses {204 {:body nil?}}
-              :summary   "Create account"}}]]))
+      {:post   {:handler   (account/create-account! db)
+                :responses {204 {:body nil?}}
+                :summary   "Create account"}
+       :delete {:handler   (account/delete-account! db)
+                :responses {204 {:body nil?}}
+                :summary   "Delete account"}}]]))

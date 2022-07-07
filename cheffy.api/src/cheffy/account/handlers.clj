f8f60b6fb471eb72791221e67deedd445218bef8
felipe-basina <felipe.basina@gmail.com>
2022-07-07 11:08:02 -0300

    Adicionado handler para remover conta.

--- cheffy.api/src/cheffy/account/handlers.clj
+++ cheffy.api/src/cheffy/account/handlers.clj
@@ -1,5 +1,6 @@
 (ns cheffy.account.handlers
   (:require [ring.util.response :as rr]
+            [clj-http.client :as http]
             [cheffy.account.db :as account-db]))
 
 (defn create-account!
@@ -10,3 +11,14 @@
                                       :name    name
                                       :picture picture})
       (rr/status 204))))
+
+(defn delete-account!
+  [db]
+  (fn [request]
+    (let [uid (-> request :claims :sub)
+          delete-auth0-account! (http/delete
+                                  (str "https://dev-fansjs74.us.auth0.com/api/v2/users/" uid)
+                                  {:headers {"Authorization" (str "Bearer " (auth0/get-management-token))}})]
+      (when (= 204 (:status delete-auth0-account!))
+        (account-db/delete-account! db {:uid uid})
+        (rr/status 204)))))

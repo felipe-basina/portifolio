36a874731b5715670e58d295af0a2958ad3b8bef
felipe-basina <felipe.basina@gmail.com>
2022-07-07 11:33:44 -0300

    Adicionado operacao para recuperar token a ser utilizado na gestao de contas.

--- cheffy.api/src/cheffy/auth0.clj
+++ cheffy.api/src/cheffy/auth0.clj
@@ -17,5 +17,20 @@
        (m/decode-response-body)
        :access_token))
 
+(defn get-management-token
+  []
+  (->> {:throw-exceptions false                             ; To show errors in a body response fashion
+        :content-type     :json
+        :cookie-policy    :standard
+        :body             (m/encode "application/json"
+                                    {:client_id     "3d5wra4p2aisoNxKJLIASj6huVJJEIp6"
+                                     :client_secret "LBlgmhL8VrEhwhESwYWp9ySs2Hi6dvppb3VUYtIITkcbnxJMBnDPo9QsnTK6JGaJ"
+                                     :audience      "https://dev-fansjs74.us.auth0.com/api/v2/"
+                                     :grant_type    "client_credentials"})}
+       (http/post "https://dev-fansjs74.us.auth0.com/oauth/token")
+       (m/decode-response-body)
+       :access_token))
+
 (comment
+  (get-management-token)
   (get-test-token))

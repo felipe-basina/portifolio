303b2abf24d28bc47ed5dd5cdb5e99f26126cfa2
felipe-basina <felipe.basina@gmail.com>
2022-07-07 19:26:35 -0300

    Refatoracao de componentes de testes; separacao de funcoes de fixture nos respectivos namespaces.

--- cheffy.api/test/cheffy/test_system.clj
+++ cheffy.api/test/cheffy/test_system.clj
@@ -3,23 +3,46 @@
             [integrant.repl.state :as state]
             [muuntaja.core :as m]
             [ring.mock.request :as mock]
-            [cheffy.auth0 :as auth0]))
+            [cheffy.auth0 :as auth0]
+            [clj-http.client :as http]))
+
+(defn get-test-token
+  [email]
+  (->> {:content-type  :json
+        :cookie-policy :standard
+        :body          (m/encode "application/json"
+                                 {:client_id  "gUSjqIkdJVSyCkBgAg3Nmxqq2c5U5Ark"
+                                  :audience   "https://dev-fansjs74.us.auth0.com/api/v2/"
+                                  :grant_type "password"
+                                  :username   email
+                                  :password   "Testing#00"
+                                  :scope      "openid profile email"})}
+       (http/post "https://dev-fansjs74.us.auth0.com/oauth/token")
+       (m/decode-response-body)
+       :access_token))
+
+(defn create-auth0-user
+  [{:keys [connection email password]}]
+  (->> {:headers          {"Authorization" (str "Bearer " (auth0/get-management-token))}
+        :throw-exceptions false                             ; To show errors in a body response fashion
+        :content-type     :json
+        :cookie-policy    :standard
+        :body             (m/encode "application/json" {:connection connection
+                                                        :email      email
+                                                        :password   password})}
+       (http/post "https://dev-fansjs74.us.auth0.com/api/v2/users")
+       (m/decode-response-body)))
 
 (def token (atom nil))
-
-(defn token-fixture
-  [f]
-  (reset! token (auth0/get-test-token))
-  (f)
-  (reset! token nil))
 
 (defn test-endpoint
   ([method uri]
    (test-endpoint method uri nil))
   ([method uri opts]
    (let [app (-> state/system :cheffy/app)
+         email "testing@cheffy.app"
          request (app (-> (mock/request method uri)
-                          (cond-> (:auth opts) (mock/header :authorization (str "Bearer " (or @token (auth0/get-test-token))))
+                          (cond-> (:auth opts) (mock/header :authorization (str "Bearer " (or @token (get-test-token email))))
                                   (:body opts) (mock/json-body (:body opts)))))]
      (update request :body (partial m/decode "application/json"))))) ;; Equivalent to (fn [data] (m/decode "application/json" data))
 

44376b740d86283c5f130cf96a48c967554ade83
felipe-basina <felipe.basina@gmail.com>
2022-07-07 12:38:25 -0300

    Adicionado handler para atualizar perfil da conta.

--- cheffy.api/src/cheffy/account/handlers.clj
+++ cheffy.api/src/cheffy/account/handlers.clj
@@ -2,7 +2,8 @@
   (:require [ring.util.response :as rr]
             [clj-http.client :as http]
             [cheffy.account.db :as account-db]
-            [cheffy.auth0 :as auth0]))
+            [cheffy.auth0 :as auth0]
+            [muuntaja.core :as m]))
 
 (defn create-account!
   [db]
@@ -12,6 +13,20 @@
                                       :name    name
                                       :picture picture})
       (rr/status 204))))
+
+(defn update-role-to-cook!
+  []
+  (fn [request]
+    (let [uid (-> request :claims :sub)
+          token (auth0/get-management-token)]
+      (->> {:headers          {"Authorization" (str "Bearer " token)}
+            :cookie-policy    :standard
+            :content-type     :json
+            :throw-exceptions false
+            :body             (m/encode
+                                "application/json"
+                                {:roles [(auth0/get-role-id token)]})}
+           (http/post (str "https://dev-fansjs74.us.auth0.com/api/v2/users/" uid "/roles"))))))
 
 (defn delete-account!
   [db]
